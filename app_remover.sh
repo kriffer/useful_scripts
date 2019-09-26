6bd2c634b97532dcc1ff4d6c9750bd5b7486e46e
kriffer <akravets@azul.com>
2019-09-26 19:13:36 +0300

    9/26: added app_remover.sh to remove apps from Mac and Linux env. Added gitignore

---
+++ app_remover.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script is intended to be useful while applications removing in Linux and Mac environment.
+# usage:  sudo ./app_remover.sh <app_name>
+
+APP=$1
+IFS=$'\n'
+RM=$(which rm)
+declare -a PATHS=($(find / -name "*$APP*" -type f -o -name "*$APP*" -type d 2>&1 | grep -v "find:"))
+# shellcheck disable=SC2068
+for PATH in in ${PATHS[@]}; do
+
+  while read -p "Deleting: $PATH. Are you sure ? (y/n): " answer; do
+    if [[ $answer =~ ^[Yy]$ ]] || [[ $answer =~ ^[Nn]$ ]]; then
+      break
+    else continue
+    fi
+  done
+  if [[ $answer =~ ^[Yy]$ ]]; then
+    $RM -rf $PATH
+  else
+    continue
+  fi
+done
